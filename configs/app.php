b388f1ad5e92a00be6fd5381e1eef540745169fd
Maxim Vadimovich <maxim@example.com>
2025-12-14 21:14:44 +0500

    chore: full cleanup, start project from scratch

--- configs/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    "domains" => "localhost/",
-    "site_name" => "LovlyEngine",
-];
--- configs/twig.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'views_path' => ROOT . '/views',
-    'cache_path' => ROOT . '/storage/cache/views',
-    'debug' => true,
-    'auto_reload' => true,
-];

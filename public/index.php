843aca40acd89a40b10943b9d6f943ca433029f3
Maxim Vadimovich <maxim@example.com>
2025-12-14 21:24:29 +0500

    Dev Stable

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Copyright (c) 2025 HivePHP OldVkDev
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
+ *
+ */
+ 
